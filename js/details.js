ab7405c48ab0050fe8586dfb9f8693d9a48e4d0f
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-14 20:53:32 -0600

    feat: add details page logic to load the data from the api

--- js/details.js
+++ js/details.js
@@ -1,9 +1,8 @@
 import HttpClient from "./httpClient.js";
-const id = new URLSearchParams(window.location.search).get("id");
+const currentPostId = new URLSearchParams(window.location.search).get("id");
 const detailsContainer = document.querySelector(".post-details-container");
-// let author;
-let users;
 let comments;
+let users;
 let tags;
 
 const httpClient = new HttpClient(
@@ -12,13 +11,64 @@ const httpClient = new HttpClient(
 
 window.addEventListener("DOMContentLoaded", async (e) => {
   users = await httpClient.get("/users");
-  comments = await httpClient.get("/comments");
   tags = await httpClient.get("/tags");
+  comments = await httpClient.get("/comments");
   renderDetails();
 });
 
+document.body.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const user = e.target.user.value;
+  const commentText = e.target.comment.value.trim();
+  const commentsContainer = document.querySelector(
+    ".post-details__comments-list"
+  );
+  const commentsContainerLabel = document.querySelector(".post-details__label");
+
+  const comment = {
+    comment: commentText,
+    postId: +currentPostId,
+    user: +user,
+  };
+
+  e.target.reset();
+  e.target.newCommentSubmit.setAttribute("disabled", true);
+
+  const newComment = await httpClient.post("/comments/", comment);
+  const updatedComments = await httpClient.get("/comments");
+  comments = updatedComments;
+
+  appendComment(newComment, commentsContainer);
+  const commentsByPost = comments.filter(
+    (comment) => +comment.postId === +currentPostId
+  );
+  commentsContainerLabel.textContent = `All comments (${commentsByPost.length})`;
+});
+
+document.body.addEventListener("input", (e) => {
+  const submitCommentBtn = document.querySelector(".new-comment__submit");
+  if (e.target.id === "comment") {
+    if (e.target.value.trim() !== "") {
+      submitCommentBtn.removeAttribute("disabled");
+    } else {
+      submitCommentBtn.setAttribute("disabled", true);
+    }
+  }
+});
+
+document.body.addEventListener("click", async (e) => {
+  if (e.target.id === "post-details-delete") {
+    if (
+      confirm("Do you really want to delete this post?\nThis can´t be undone.")
+    ) {
+      await httpClient.delete(`/posts/${currentPostId}`);
+      window.location.replace("/index.html");
+    }
+  }
+});
+
 async function renderDetails() {
-  const post = await httpClient.get(`/posts/${id}`);
+  const post = await httpClient.get(`/posts/${currentPostId}`);
   renderPostDetailsMarkup(post);
 }
 
@@ -48,7 +98,16 @@ async function renderPostDetailsMarkup(post) {
 
   const commentsByPost = comments.filter((comment) => comment.postId === id);
 
-  const postDetailsMarkup = `<article class="post-details">
+  const postDetailsMarkup = `
+  <header class="post-details-header">
+        <a id="post-details-delete" class="post-details-delete">
+          Delete Post <i class="fa-solid fa-trash-can"></i>
+        </a>
+        <a href="./edit.html?id=${currentPostId}" class="post-details-edit">
+          Edit Post <i class="fa-solid fa-file-pen"></i>
+        </a>
+      </header>
+  <article class="post-details">
   <div class="post-details__image-container">
     <img
       src="${image}"
@@ -81,24 +140,32 @@ async function renderPostDetailsMarkup(post) {
   </div>
   <div class="post-details__comments">
     <p class="post-details__label">All comments (${commentsByPost.length})</p>
-    <form class="post-details__new-comment">
+    <form id="post-details__new-comment" class="post-details__new-comment">
       <select class="new-comment__author" name="user" id="user">
       ${usersOptions.join("")}
       </select>
       <textarea
         class="new-comment__text"
         name="comment"
-        id="1"
+        id="comment"
         cols="30"
         rows="6"
       ></textarea>
-      <button class="new-comment__submit">Submit</button>
+      <button id="newCommentSubmit" class="new-comment__submit" disabled>Submit</button>
     </form>
+    <div class="post-details__comments-list">
     ${commentsByPost.map((comment) => createCommentMarkup(comment.id)).join("")}
+    </div>
   </div>
 </article>`;
 
   detailsContainer.insertAdjacentHTML("afterbegin", postDetailsMarkup);
+}
+
+function appendComment(comment, commentsContainer) {
+  const commentMarkup = createCommentMarkup(comment.id);
+
+  commentsContainer.insertAdjacentHTML("beforeend", commentMarkup);
 }
 
 function createCommentMarkup(commentId) {
@@ -119,11 +186,7 @@ function createCommentMarkup(commentId) {
 }
 
 // AUN FALTA
-// 1 - estilizar la pagina de details
-// 2 - agregar los comentario del post dinamicamente
+
 // 3 - que el usuario pueda darle like al post y se sume enviando una peticion patch, con un efecto de toggle al clickar en el icono de like usando debounce
-// 4 - aplicar debounce al formulario de busqueda
-// 5 - que el usuario pueda agregar un comentario seleccionando al usuario del select y este se guarde mediante una peticion post
-// 6 - que el usuario pueda borrar un post mediante un boton con un icono de basurero y le pida confirmacion antes
-// 7 - que el usuario pueda editar un post con clickar un boton y que lo lleve al formulario y precargue los datos en los campos del mismo y que al darle guardar se envien los cambios mediante una peticion patch
+
 // 8 - usar el patron factory en la app
